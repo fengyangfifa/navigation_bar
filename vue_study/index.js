88341a13fe1975177d1cdd1d39b90b7a9333cb83
fengyangfifa <2314034614@qq.com>
2019-11-06 18:17:34 +0800

    vue 学习03

--- vue_study/index.js
+++ vue_study/index.js
@@ -1,14 +1,44 @@
-var obj = {
-    rawHtml: "bar"
-};
-// Object.freeze(obj);
 var app = new Vue({
-    el:"#app",
-    data: obj,
-    // created() {
-    //     console.log('a is: ' + this.foo);
-    // }
-    created: function () {
-        console.log('a is: ' + this.foo);
+    el: "#example",
+    data: {
+        message: "hello"
+    },
+    computed: {
+        reversedMessage: function () {
+            return this.message.split('').reverse().join('');
+        }
+    },
+    methods: {
+        now: function () {
+            return Date.now();
+        }
+    }
+});
+
+var app2 = new Vue({
+    el: "#demo",
+    data: {
+        firstName: "Foo",
+        lastName: "Bar",
+    },
+    // watch: {
+    //     firstName: function (val) {
+    //         this.fullName = val + ' ' + this.lastName;
+    //     },
+    //     lastName: function (val) {
+    //         this.fullName = this.firstName + ' ' + val;
+    //     }
+    // },
+    computed: {
+        fullName: {
+            get: function () {
+                return this.firstName+ " " + this.lastName;
+            },
+            set: function (newvalue) {
+                var names = newvalue.split(" ");
+                this.firstName = names[0];
+                this.lastName = names[names.length - 1];
+            }
+        } 
     }
 });
