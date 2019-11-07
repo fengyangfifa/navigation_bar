a43c63a5267e7a2bc653ebd2fe89aba969606550
fengyangfifa <2314034614@qq.com>
2019-11-07 19:02:10 +0800

    vue 学习04

--- vue_study/index.js
+++ vue_study/index.js
@@ -1,17 +1,39 @@
+Vue.component('todo-item', {
+    template: '\
+        <li>\
+            {{ title }}\
+            <button v-on:click="$emit(\'remove\')">Remove</button>\
+        </li>\
+    ',
+    props: ['title']
+});
 var app = new Vue({
-    el: "#app-3",
-    // data: {
-    //     styleObject: {
-    //         color: 'red',
-    //         fontSize: '30px'    
-    //     }
-    // }
+    el: "#todo-list-example",
     data: {
-        baseStyles:{
-            color: 'red'
-        },
-        display:{
-            display: ['-webkit-box', '-ms-flexbox', 'flex']
+        newTodoText: '',
+        todos: [
+            {
+                id: 1,
+                title: 'Do the dishes'
+            },
+            {
+                id: 2,
+                title: 'Take out the trash'
+            },
+            {
+                id: 3,
+                title: 'Mow the lawn'
+            }
+        ],
+        nextTodoId: 4
+    },
+    methods: {
+        addNewTodo: function () {
+            this.todos.push({
+                id: this.nextTodoId++,
+                title: this.newTodoText
+            });
+            this.newTodoText = '';
         }
-    }
+    },
 });
