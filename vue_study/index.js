5054fce59a147ba6ea6fad8c66be515553dc77ab
fengyangfifa <2314034614@qq.com>
2019-11-08 15:20:07 +0800

    vue 学习04

--- vue_study/index.js
+++ vue_study/index.js
@@ -1,73 +1,40 @@
-// Vue.component('button-counter', {
-//     data: function () {
-//         return {
-//             count: 0
-//         }
-//     },
-//     template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
-// });
-Vue.component("blog-post", {
-    props: ["post"],
-    template: `<div class=blog-post>
-    <h3>{{ post.title }}</h3>
-    <button v-on:click="$emit('enlarge-text', 0.1)">Enlarge text</button>
-    <div v-html="post.content"></div>
-    </div>`
-});
-Vue.component("custom-input", {
-    props: ['value'],
-    template: `
-        <input
-            v-bind:value="value"
-            v-on:input="$emit('input', $event.target.value)"
-        >
-    `
-});
-Vue.component('alert-box', {
-    template: `
-      <div class="demo-alert-box">
-        <strong>Error!</strong>
-        <slot></slot>
-      </div>
-    `
-});
-Vue.component('tab-home', {
-    template: '<div>Home component</div>'
-});
-Vue.component('tab-posts', {
-    template: '<div>Posts component</div>'
-});
-Vue.component('tab-archive', {
-    template: '<div>archive component</div>'
-});
-
-var app1 = new Vue({
-    el: "#dynamic-component-demo",
+Vue.component('todo-item', {
+    template: '\
+      <li>\
+        {{ title }}\
+        <button v-on:click="$emit(\'remove\')">Remove</button>\
+      </li>\
+    ',
+    props: ['title']
+  })
+  
+  new Vue({
+    el: '#todo-list-example',
     data: {
-        currentTab: "Home",
-        tabs: ["Home", "Posts", "Archive"]
-    },
-    computed: {
-        currentTabComponent: function () {
-            return 'tab-' + this.currentTab.toLowerCase();
+      newTodoText: '',
+      todos: [
+        {
+          id: 1,
+          title: 'Do the dishes',
+        },
+        {
+          id: 2,
+          title: 'Take out the trash',
+        },
+        {
+          id: 3,
+          title: 'Mow the lawn'
         }
-    },
-});
-
-var app = new Vue({
-    el: "#components-demo",
-    data: {
-        posts: [
-            { id: 1, title: "My journey with Vue", content: "aaa" },
-            { id: 2, title: "Blogging with Vue", content: "bbb" },
-            { id: 3, title: "Why Vue is so fun", content: "ccc" }
-        ],
-        postFontSize: 1,
-        searchText: ''
+      ],
+      nextTodoId: 4
     },
     methods: {
-        onEnlargeText: function (enlargeAmount) {
-            this.postFontSize += enlargeAmount;
-        }
-    },
-});
+      addNewTodo: function () {
+        this.todos.push({
+          id: this.nextTodoId++,
+          title: this.newTodoText
+        })
+        this.newTodoText = ''
+      }
+    }
+  })
