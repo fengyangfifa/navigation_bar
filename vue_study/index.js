b0d881244ed7082f2014f048144ec4f7134c18c1
fengyangfifa <2314034614@qq.com>
2019-11-11 09:54:24 +0800

    Vue 学习05

--- vue_study/index.js
+++ vue_study/index.js
@@ -1,6 +1,36 @@
-var example = new Vue({
+Vue.component('submit-button', {
+	template: `
+		<div class="container">
+			<header>
+				<slot name="header">header</slot>
+			</header>
+			<main>
+				<slot>main</slot>
+			</main>
+			<footer>
+				<slot name="footer">footer</slot>
+			</footer>
+		</div>
+	`
+});
+Vue.component('current-user', {
+	data() {
+		return {
+			user: {
+				firstName: 'fe',
+				lastName: 'ya'
+			}
+		}
+	},
+	template:`
+		<span>
+			<slot v-bind:user="user">{{ user.lastName }}</slot>
+		</span>
+	`
+});
+var app = new Vue({
 	el: "#example-2",
 	data: {
-		msg: '1'
+
 	}
 });
