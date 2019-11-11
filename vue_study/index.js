03904c651ad84d748045c700965a9cdcdb313db7
fengyangfifa <2314034614@qq.com>
2019-11-12 00:29:41 +0800

    vue 学习05

--- vue_study/index.js
+++ vue_study/index.js
@@ -1,81 +1,50 @@
-Vue.component('submit-button', {
-	template: `
-		<div class="container">
-			<header>
-				<slot name="header">header</slot>
-			</header>
-			<main>
-				<slot>main</slot>
-			</main>
-			<footer>
-				<slot name="footer">footer</slot>
-			</footer>
-		</div>
-	`
-});
-Vue.component('current-user', {
-	data() {
-		return {
-			user: {
-				firstName: 'fe',
-				lastName: 'ya'
-			}
-		}
-	},
-	template: `
-		<span>
-			<slot v-bind:user="user">{{ user.lastName }}</slot>
-		</span>
-	`
-});
-var app = new Vue({
-	el: "#example-2",
+new Vue({
+	el: '#example-1',
 	data: {
-
+		show: true
 	}
 });
-// function test(resolve, reject) {
-// 	var timeOut = Math.random() * 2;
-// 	console.log('set timeout to: ' + timeOut + ' seconds.');
-// 	setTimeout(function () {
-// 		if(timeOut < 1){
-// 			console.log("call resolve()...");
-// 			resolve('200 OK');
-// 		}else{
-// 			console.log('call reject()...');
-// 			reject('timeout in ' + timeOut + ' seconds.');
-// 		}
-// 	}, timeOut*10);
-// }
-// new Promise(test).then(function (result) {
-// 	console.log('成功：' + result);
-// }).catch(function (reason) {
-// 	console.log('失败：' + reason);
-// })
-function multiply(input) {
-	return new Promise(function (resolve, reject) {
-		console.log('calculating ' + input + ' x ' + input + '...');
-		setTimeout(resolve, 500, input * input);
-	});
-}
-
-// 0.5秒后返回input+input的计算结果:
-function add(input) {
-	return new Promise(function (resolve, reject) {
-		console.log('calculating ' + input + ' + ' + input + '...');
-		setTimeout(resolve, 500, input + input);
-	});
-}
-
-var p = new Promise(function (resolve, reject) {
-	console.log('start new Promise...');
-	resolve(123);
+new Vue({
+	el: '#example-2',
+	data: {
+		show: true
+	}
 });
-
-p.then(multiply)
-	.then(add)
-	.then(multiply)
-	.then(add)
-	.then(function (result) {
-		console.log('Got value: ' + result);
-	});
+new Vue({
+	el: '#example-3',
+	data: {
+		show: true
+	}
+});
+new Vue({
+	el: '#transition-components-demo',
+	data: {
+		view: 'v-a'
+	},
+	components: {
+		'v-a': {
+			template: '<div>Component A</div>'
+		},
+		'v-b': {
+			template: '<div>Component B</div>'
+		}
+	}
+});
+new Vue({
+	el: '#list-demo',
+	data: {
+		items: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+		nextNum: 10
+	},
+	methods: {
+		randomIndex: function () {
+			return Math.floor(Math.random() * this.items.length)
+		},
+		add: function () {
+			this.items.splice(this.randomIndex(), 0, this.nextNum++)
+		},
+		remove: function () {
+			this.items.splice(this.randomIndex(), 1)
+		},
+	}
+});
