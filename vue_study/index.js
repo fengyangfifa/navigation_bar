1ec7d4704a21b8ee0774427e7396ee4ab3e2bae5
fengyangfifa <2314034614@qq.com>
2019-11-12 18:45:49 +0800

    vue 学习06

--- vue_study/index.js
+++ vue_study/index.js
@@ -1,50 +1,19 @@
 new Vue({
-	el: '#example-1',
+	el: '#animated-number-demo',
 	data: {
-		show: true
-	}
-});
-new Vue({
-	el: '#example-2',
-	data: {
-		show: true
-	}
-});
-new Vue({
-	el: '#example-3',
-	data: {
-		show: true
-	}
-});
-new Vue({
-	el: '#transition-components-demo',
-	data: {
-		view: 'v-a'
+		number: 0,
+		tweenedNumber: 0
 	},
-	components: {
-		'v-a': {
-			template: '<div>Component A</div>'
-		},
-		'v-b': {
-			template: '<div>Component B</div>'
+	computed: {
+		animatedNumber: function () {
+			return this.tweenedNumber.toFixed(0);
 		}
-	}
-});
-new Vue({
-	el: '#list-demo',
-	data: {
-		items: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-		nextNum: 10
 	},
-	methods: {
-		randomIndex: function () {
-			return Math.floor(Math.random() * this.items.length)
-		},
-		add: function () {
-			this.items.splice(this.randomIndex(), 0, this.nextNum++)
-		},
-		remove: function () {
-			this.items.splice(this.randomIndex(), 1)
-		},
+	watch: {
+		number: function (newValue) {
+			TweenLite.to(this.$data, 0.5, {
+				tweenedNumber: newValue
+			});
+		}
 	}
 });
