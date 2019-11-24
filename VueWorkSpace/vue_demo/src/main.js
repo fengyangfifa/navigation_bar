a8ee832d0a89fa68b09cf1250a08adc86cf60cf7
fengyangfifa <2314034614@qq.com>
2019-11-24 17:33:14 +0800

    vue vue_demo 06

--- VueWorkSpace/vue_demo/src/main.js
+++ VueWorkSpace/vue_demo/src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import './base.css'
-
 new Vue({
-	el: "#app",
-	components: {
-		App
-	},
-	template: '<App/>'
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App/>'
 });
---
+++ VueWorkSpace/vue_demo/src3/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <!-- 方法1props -->
+      <!-- <TodoHeader :addTodo="addTodo"/> -->
+
+      <!-- 方法2自定义事件 -->
+      <!-- <TodoHeader @addTodo="addTodo"/> -->
+      <TodoHeader ref="header"/>
+
+      <!-- <TodoList :todos="todos" :deleteTodo="deleteTodo" /> -->
+      <TodoList :todos="todos" />
+      <TodoFooter :todos="todos" :deleteCompleteTodos="deleteCompleteTodos" 
+      :selectAllTodos="selectAllTodos" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+import storageUtil from './util/storageUtil.js';
+
+import PubSub from 'pubsub-js';
+
+export default {
+  data() {
+    return {
+      todos: storageUtil.readTodos()
+    }
+  },
+  watch: {
+    // 深度监视数据
+    todos: {
+      deep: true,
+      // handler: function (value) {
+      //   storageUtil.saveTodos(value);
+      // }
+      // 或
+      handler: storageUtil.saveTodos
+    }
+  },
+  mounted() { //执行异步事件
+    // 给<TodoHeader ref="header"/>绑定addTodo事件监听
+    this.$refs.header.$on('addTodo', this.addTodo);
+
+    // 订阅消息
+    PubSub.subscribe("deleteTodo", (msg, index) => {
+      this.deleteTodo(index);
+    })
+  },
+  methods: {
+    addTodo (todo) {
+      this.todos.unshift(todo);
+    },
+    deleteTodo (index) {
+      this.todos.splice(index, 1);
+    },
+    deleteCompleteTodos () {
+      this.todos = this.todos.filter(todo => !todo.complete);
+    },
+    selectAllTodos (check) {
+      this.todos.forEach(todo => todo.complete = check);
+    }
+  },
+  components: {
+    TodoHeader,
+    TodoList,
+    TodoFooter
+  }
+};
+</script>
+
+<style>
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #dddddd;
+  border-radius: 5px;
+}
+</style>
