09a4b764ccb90a56d772c21aac727f8df6548921
fengyangfifa <2314034614@qq.com>
2019-12-08 23:13:34 +0800

    vue shop 02

--- VueWorkSpace/gshop/src/App.vue
+++ VueWorkSpace/gshop/src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view></router-view>
+    <FooterGuide/>
   </div>
 </template>
 
 <script>
+import FooterGuide from './components/FooterGuide/FooterGuide'
 export default {
-  name: 'App'
-}
+  components: {
+    FooterGuide
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5
 </style>
--- VueWorkSpace/gshop/src/router/index.js
+++ VueWorkSpace/gshop/src/router/index.js
@@ -1,15 +1,38 @@
+// 路由器对象模块
+
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VueRouter from 'vue-router'
+import MSite from '../pages/MSite/MSite'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+import Search from '../pages/Search/Search'
 
-Vue.use(Router)
+// 声明使用插件
+Vue.use(VueRouter);
 
-export default new Router({
+
+export default new VueRouter({
   routes: [
     {
+      path: '/msite',
+      component: MSite
+    },
+    {
+      path: '/search',
+      component: Search,
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      // 默认路由
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/msite'      
     }
   ]
-})
+});
