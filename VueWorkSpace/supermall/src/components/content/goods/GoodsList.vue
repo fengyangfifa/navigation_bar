e6b95e40becf62ddb7e9b96947f5f700d1fb8000
fengyangfifa <2314034614@qq.com>
2020-01-06 23:14:24 +0800

    vue 15

---
+++ VueWorkSpace/supermall/src/components/content/goods/GoodsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="goods-list">
+    <goods-list-item class="item" v-for="(item, index) in goods" :key="index" :goods-item="item"></goods-list-item>
+  </div>
+</template>
+
+<script>
+import GoodsListItem from './GoodsListItem'
+
+export default {
+  props: {
+    goods: {
+      type: Array,
+      default () {
+        return [];
+      }
+    }
+  },
+  components: {
+    GoodsListItem
+  }
+}
+</script>
+
+<style scoped>
+.goods-list {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 5px;
+  justify-content: space-around;
+}
+
+.goods-list .item {
+  width: 48%;
+}
+</style>
--- VueWorkSpace/supermall/src/router/index.js
+++ VueWorkSpace/supermall/src/router/index.js
@@ -24,7 +24,7 @@ const router = new VueRouter({
       component: Cart
     },
     {
-      path: '/cartgory',
+      path: '/category',
       component: Category
     },
     {
