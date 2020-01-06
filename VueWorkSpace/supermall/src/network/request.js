2d3cf62cf1afb14dac094e6353a3610077aee06a
fengyangfifa <2314034614@qq.com>
2020-01-06 14:37:04 +0800

    vue 14

---
+++ VueWorkSpace/supermall/src/network/request.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+export function request(config) {
+  // axios实例
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000/api/w1',
+    timeout: 5000
+  });
+
+  //请求拦截器
+  instance.interceptors.request.use(config => {
+    return config;
+  }, err => {
+    return err;
+  });
+
+  // response拦截器
+  instance.interceptors.response.use(res => {
+    return res.data;
+  }, err => {
+    return err;
+  });
+
+  // 真正发送网络请求
+  return instance(config);
+}
--- VueWorkSpace/supermall/src/views/home/Home.vue
+++ VueWorkSpace/supermall/src/views/home/Home.vue
@@ -3,15 +3,33 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
+
+import {getHomeMultidata} from 'network/home'
 
 export default {
   components: {
-    NavBar
+    NavBar,
+    HomeSwiper
+  },
+  data () {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    // 请求首页数据
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
   }
 }
 </script>
