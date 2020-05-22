4a291450b9b0517183feaf49d2c4a53ff7ceaf9f
fengyangfifa <2314034614@qq.com>
2020-05-22 23:25:44 +0800

    小程序03

--- ReactWorkSpace/reactStudy/like_button.js
+++ ReactWorkSpace/reactStudy/like_button.js
@@ -1,33 +1,4 @@
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false};
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked comment number' + this.props.commentID;
-    }
-
-    return e(
-      'button',
-      {onClick: () => this.setState({liked: true})},
-      'Like'
-    );
-  }
-}
-
-document.querySelectorAll('.like_button_container')
-  .forEach(domContainer => {
-    // Read the comment ID from a data-* attribute.
-    const commentID = parseInt(domContainer.dataset.commentid, 10);
-    ReactDOM.render(
-      e(LikeButton, { commentID: commentID }),
-      domContainer
-    );
-  });
-// npx create-react-app my-app
-// cd my-app
-// npm start
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
