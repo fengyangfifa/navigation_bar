7b8852a34e3a7505c11a63926e082951a0699ff1
fengyangfifa <2314034614@qq.com>
2020-06-07 23:27:30 +0800

    react基础 01

--- ReactWorkSpace/reactStudy/like_button.jsx
+++ ReactWorkSpace/reactStudy/like_button.jsx
@@ -1,3 +1,5 @@
+import { func } from "prop-types";
+
 var name = 'Josh Perez';
 var element = <h1>Hello, {name}</h1>;
 
@@ -5,3 +7,37 @@ ReactDOM.render(
   element,
   document.getElementById('root')
 )
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleDateString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById('root'));
